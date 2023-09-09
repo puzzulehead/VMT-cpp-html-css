1ae4638f97d0823d54fb9f41aed63fe1cab1d081
Vikrant Kumar <133192786+puzzulehead@users.noreply.github.com>
2023-09-09 05:55:38 +0530

    Create vik.java

---
+++ vik.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class inputfromuser {
+    public static void main(String[] args) {
+        System.out.println("taking input from the user");
+        Scanner sc=new Scanner(System.in);//system.in specifies that user is taking input from the keyboard
+//        System.out.println("enter the first number");
+//        int a=sc.nextInt();
+//        System.out.println("enter the second number");
+//        int b=sc.nextInt();
+//        int sum=a+b;
+//        System.out.println("The sum of this number is");
+//        System.out.println(sum);
+        String str=sc.nextLine();
+        System.out.println(str);
+
+    }
+
+}
